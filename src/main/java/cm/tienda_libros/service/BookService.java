4aadde8a9dd22794ba552f1289fba96ac50ebca4
ChristianMallo <118746425+ChristianMallo@users.noreply.github.com>
2024-10-19 19:37:52 +0200

    modification in service

--- src/main/java/cm/tienda_libros/service/BookService.java
+++ src/main/java/cm/tienda_libros/service/BookService.java
@@ -3,9 +3,11 @@ package cm.tienda_libros.service;
 import cm.tienda_libros.model.Book;
 import cm.tienda_libros.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BookService implements IBookService{
 
     @Autowired
